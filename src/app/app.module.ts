432ea26a52307ebcbce62e45f0c8ea7c483453dd
Irina Vlasceanu <irinav0402@gmail.com>
2019-08-17 16:35:29 +0300

    (chore) setup admin module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AdminModule } from './admin/admin.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -22,6 +23,7 @@ import { ContactComponent } from './contact/contact.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    AdminModule,
     BrowserAnimationsModule
   ],
   providers: [],
