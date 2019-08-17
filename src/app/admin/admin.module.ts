b7f384de0a6ba363e4a0d89bf0a02ab24d6e4a13
Irina Vlasceanu <irinav0402@gmail.com>
2019-08-17 16:56:13 +0300

    feat(admin): add admin nested routes

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdminRoutingModule } from './admin-routing.module';
 import { ProjectListComponent } from './project-list/project-list.component';
 import { ProjectCreateComponent } from './project-create/project-create.component';
 import { ProjectUpdateComponent } from './project-update/project-update.component';
 import { ProjectComponent } from './project/project.component';
+import { ImportsModule } from '../imports/imports.module';
 
 
 
 @NgModule({
   declarations: [ProjectListComponent, ProjectCreateComponent, ProjectUpdateComponent, ProjectComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    AdminRoutingModule,
+    ImportsModule
   ]
 })
 export class AdminModule { }
